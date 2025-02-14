0a75b5e6220cd5de01e47080206c8370409dfbf0
Bronze <defrianto.bronze@gmail.com>
2025-02-15 05:02:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function enableScroll() {
+    const container = document.querySelector('.content-container');
+    container.style.overflowY = 'auto';
+    document.getElementById('content2').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Mencegah bounce effect pada iOS Safari
+document.addEventListener('touchmove', function(e) {
+    if (document.querySelector('.content-container').style.overflowY !== 'auto') {
+        e.preventDefault();
+    }
+}, { passive: false });
+
+// Reset scroll position saat refresh
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+};
